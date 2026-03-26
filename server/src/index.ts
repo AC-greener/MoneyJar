635c1431feea6fce07e3ab97118e2e57bbdcfc91
zhutongtong <15929774304@163.com>
2026-03-26 08:47:37 +0800

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/d1";
+import { posts } from './db/schema';
 
 type Bindings = {
-  AI: Ai
-}
+  AI: Ai;
+  DB: D1Database;
+};
 
 // 2. 将类型传给 Hono 实例
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', async (c) => {
-  // 3. 通过 c.env 获取绑定
-  const response = await c.env.AI.run("@cf/meta/llama-3.1-8b-instruct" as any, {
-    prompt: "What is the origin of the phrase Hello, World",
+app
+  .get("/test", async (c) => {
+    // 3. 通过 c.env 获取绑定
+    const response = await c.env.AI.run(
+      "@cf/meta/llama-3.1-8b-instruct" as any,
+      {
+        prompt: "What is the origin of the phrase Hello, World",
+      }
+    );
+
+    return c.json(response); // Hono 有内置的 c.json()，比 JSON.stringify 更好用
+  })
+  .get("/posts", async (c) => {
+    const db = drizzle(c.env.DB);
+    const result = await db.select().from(posts).all();
+    return c.json(result);
   });
 
-  return c.json(response); // Hono 有内置的 c.json()，比 JSON.stringify 更好用
-})
-
-export default app
+export default app;
